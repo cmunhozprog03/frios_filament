3628fee8f026dbfefa9150d539b8ec4ec7cc6794
cmunhozprog03 <cmunhozprog03@hotmail.com>
2023-01-20 18:55:08 -0300

    category

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\{
+    Category
+};
+use App\Observers\{
+    CategoryObserver
+};
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -27,7 +33,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Category::observe(CategoryObserver::class);
     }
 
     /**
--- database/migrations/2023_01_20_185904_create_quotes_table.php
+++ database/migrations/2023_01_20_185904_create_quotes_table.php
@@ -13,13 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        // 'name', 'slug', 'active', 'image'
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('quotes', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('slug')->unique();
-            $table->boolean('active')->default(1);
-            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
@@ -31,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('quotes');
     }
 };
